320b48bf1335ac2a35f35c671950061aa9ce13b7
bergony.bandeira <bergony.bandeira@quark.tec.br>
2022-11-03 07:30:24 -0300

    [bergony] Reactive Forms, Form: controls handling erros input componets.

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent  {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ] )
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ])
+  age = new FormControl('')
+  password = new FormControl('')
+  confirm_password = new FormControl('')
+  phoneNumber = new FormControl('')
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+  })
+}
