0b15add2f06934e8eb8871f2b476dc029bc19936
marlusmarcos <marlusufrn@gmail.com>
2022-12-06 16:32:26 -0300

    mock base para teste

--- src/app/livros/livros.component.ts
+++ src/app/livros/livros.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivrosComponent implements OnInit {
 
+  livros = [
+    { data: '1997', edicao: '2', titulo: 'Três porquinhos' },
+    { data: '2012', edicao: '1', titulo: 'Meu ensino médio' },
+    { data: '2021', edicao: '1', titulo: 'Estudando no imd' },
+    { data: '2022', edicao: '5', titulo: 'Como ser rico' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
