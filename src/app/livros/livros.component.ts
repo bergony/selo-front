767e8c390ebcfea2900aa3d56ba37470c1c0778d
marlusmarcos <marlusufrn@gmail.com>
2022-12-06 16:53:12 -0300

    continuacao

--- src/app/livros/livros.component.ts
+++ src/app/livros/livros.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Livro } from '../models/livro.model';
+
 @Component({
   selector: 'app-livros',
   templateUrl: './livros.component.html',
@@ -7,16 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivrosComponent implements OnInit {
 
-  livros = [
-    { data: '1997', edicao: '2', titulo: 'Três porquinhos' },
-    { data: '2012', edicao: '1', titulo: 'Meu ensino médio' },
-    { data: '2021', edicao: '1', titulo: 'Estudando no imd' },
-    { data: '2022', edicao: '5', titulo: 'Como ser rico' }
+  livros: Livro[] = [
+    { id: 1, emprestimo_id: 1, pessoa_id: 1, data_lancamento: '1997', edicao: '2', titulo: 'Três porquinhos' },
+    { id: 2, emprestimo_id: 1, pessoa_id: 1, data_lancamento: '2022', edicao: '4', titulo: 'Ingles' },
+    { id: 3, emprestimo_id: 1, pessoa_id: 1, data_lancamento: '1997', edicao: '2', titulo: 'Matematica' },
+    { id: 4, emprestimo_id: 1, pessoa_id: 1, data_lancamento: '1997', edicao: '2', titulo: 'Portugues' },
   ];
+  livro_completo = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  mostrarDetalhes(livro: Livro) {
+    this.livro_completo = `o Titulo é: ${livro.titulo} com ano ${livro.data_lancamento} e edição: ${livro.edicao} `;
   }
 
 }
---
+++ src/app/models/livro.model.ts
@@ -0,0 +1,9 @@
+export interface Livro {
+  id: number;
+  data_lancamento: string;
+  edicao: string;
+  titulo: string;
+  emprestimo_id: number;
+  pessoa_id: number;
+
+}
